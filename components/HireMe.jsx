1048165c415da28fca48c295ac40df7754427a49
Ameer Muaviya <88961101+AmeerMuaviya@users.noreply.github.com>
2023-07-15 07:43:00 +0500

    Update HireMe.jsx

--- components/HireMe.jsx
+++ components/HireMe.jsx
@@ -6,7 +6,6 @@ const HireMe = () => {
     <div className="h-full bg-blue-700/30 md:p-10 p-3 mx-auto">
         <h1 className="md:text-5xl text-3xl font-extrabold text-center">I&#39;m <span className="text-green-500">Available</span> for freelancing</h1>
         <p className="text-center md:text-xl mx-auto">I&#39;m prepared to contribute my expertise and work closely with your team to achieve project goals.</p>
-        <button></button>
   </div>
   </div>
 };
